d2c5a5b670740cd0c405b7bacaf0227846e0b79e
Kresten Laust <krestenlaust@gmail.com>
2026-04-06 11:49:27 +0200

    Update docker scripts

--- update-container.sh
+++ update-container.sh
@@ -2,11 +2,11 @@
 
 echo "Updating Wordpress container..."
 
-docker exec stregpay-checkout-wordpress-1 rm -rf /var/www/html/wp-content/plugins/stregpay-checkout
-docker exec stregpay-checkout-wordpress-1 touch /var/www/html/wp-content/plugins/stregpay-checkout
-docker cp build stregpay-checkout-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout/build
-docker cp stregpay-checkout.php stregpay-checkout-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
-docker cp blocks-integration.php stregpay-checkout-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
-docker cp payment-gateway-integration.php stregpay-checkout-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
-docker exec stregpay-checkout-wordpress-1 chown -R www-data:www-data /var/www/html/wp-content/plugins/stregpay-checkout
+docker exec stregsystem-payment-gateway-wordpress-1 rm -rf /var/www/html/wp-content/plugins/stregpay-checkout
+docker exec stregsystem-payment-gateway-wordpress-1 touch /var/www/html/wp-content/plugins/stregpay-checkout
+docker cp build stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout/build
+docker cp stregpay-checkout.php stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
+docker cp blocks-integration.php stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
+docker cp payment-gateway-integration.php stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
+docker exec stregsystem-payment-gateway-wordpress-1 chown -R www-data:www-data /var/www/html/wp-content/plugins/stregpay-checkout
 echo "Finished updating container"
