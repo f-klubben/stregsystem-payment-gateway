a8db21237e556e68f49920394eedbb2f215b793b
Kresten Laust <krestenlaust@gmail.com>
2026-09-24 15:19:26 +0200

    Update upload script

--- update-container.sh
+++ update-container.sh
@@ -2,11 +2,15 @@
 
 echo "Updating Wordpress container..."
 
-docker exec stregsystem-payment-gateway-wordpress-1 rm -rf /var/www/html/wp-content/plugins/stregpay-checkout
-docker exec stregsystem-payment-gateway-wordpress-1 mkdir /var/www/html/wp-content/plugins/stregpay-checkout
-docker cp build stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout/build
-docker cp stregpay-checkout.php stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
-docker cp blocks-integration.php stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
-docker cp payment-gateway-integration.php stregsystem-payment-gateway-wordpress-1:/var/www/html/wp-content/plugins/stregpay-checkout
-docker exec stregsystem-payment-gateway-wordpress-1 chown -R www-data:www-data /var/www/html/wp-content/plugins/stregpay-checkout
+alias podman=podman
+
+podman start stregsystem-payment-gateway_wordpress_1
+
+podman exec stregsystem-payment-gateway_wordpress_1 rm -rf /var/www/html/wp-content/plugins/stregpay-checkout
+podman exec stregsystem-payment-gateway_wordpress_1 mkdir /var/www/html/wp-content/plugins/stregpay-checkout
+podman cp build stregsystem-payment-gateway_wordpress_1:/var/www/html/wp-content/plugins/stregpay-checkout/build
+podman cp stregpay-checkout.php stregsystem-payment-gateway_wordpress_1:/var/www/html/wp-content/plugins/stregpay-checkout
+podman cp blocks-integration.php stregsystem-payment-gateway_wordpress_1:/var/www/html/wp-content/plugins/stregpay-checkout
+podman cp payment-gateway-integration.php stregsystem-payment-gateway_wordpress_1:/var/www/html/wp-content/plugins/stregpay-checkout
+podman exec stregsystem-payment-gateway_wordpress_1 chown -R www-data:www-data /var/www/html/wp-content/plugins/stregpay-checkout
 echo "Finished updating container"
